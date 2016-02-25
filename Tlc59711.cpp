165e1226b1ad65a805befb402046eb0efe6c1856
Ulrich Stern <ulrich.stern@gmail.com>
2016-02-25 13:42:06 -0500

    initial commit

---
+++ Tlc59711.cpp
@@ -0,0 +1,130 @@
+//
+// Arduino library for controlling TI's TLC59711
+//
+// 21 Feb 2016 by Ulrich Stern
+//
+
+#include "Tlc59711.h"
+#include <SPI.h>
+
+Tlc59711::Tlc59711(uint16_t numTlc, uint8_t clkPin, uint8_t dataPin):
+    numTlc(numTlc), bufferSz(14*numTlc), clkPin(clkPin), dataPin(dataPin),
+    buffer((uint16_t*) calloc(bufferSz, 2)), buffer2(0),
+    beginCalled(false) {
+  setTmgrst();
+}
+
+void Tlc59711::begin(bool useSpi, unsigned int postXferDelayMicros) {
+  end();
+  useSpi_ = useSpi;
+  postXferDelayMicros_ = postXferDelayMicros;
+  beginCalled = true;
+}
+
+void Tlc59711::beginFast(bool bufferXfer, uint32_t spiClock,
+    unsigned int postXferDelayMicros) {
+  begin(true, postXferDelayMicros);
+  bufferXfer_ = bufferXfer;
+  SPI.begin();
+  SPI.beginTransaction(SPISettings(spiClock, MSBFIRST, SPI_MODE0));
+  if (bufferXfer && !buffer2)
+    buffer2 = (uint16_t*) malloc(2*bufferSz);
+}
+void Tlc59711::beginSlow(unsigned int postXferDelayMicros, bool interrupts) {
+  begin(false, postXferDelayMicros);
+  noInterrupts = !interrupts;
+  pinMode(clkPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+}
+
+void Tlc59711::setTmgrst(bool val) {
+  // OUTTMG = 1, EXTGCK = 0, TMGRST = 0, DSPRPT = 1, BLANK = 0 -> 0x12
+  fc = 0x12 + (val ? 0x4 : 0);
+  resetBrightness();
+}
+
+void Tlc59711::setBrightness(uint16_t tlcIdx,
+    uint8_t bcr, uint8_t bcg, uint8_t bcb) {
+  if (tlcIdx < numTlc) {
+    uint32_t ms32 = (uint32_t)0x25 << 26 | (uint32_t)fc << 21 |
+      (uint32_t)bcb << 14 | (uint32_t)bcg << 7 | bcr;
+    uint16_t idx = 14*tlcIdx+12;
+    buffer[idx] = ms32;
+    buffer[++idx] = ms32 >> 16;
+  }
+}
+void Tlc59711::setBrightness(uint8_t bcr, uint8_t bcg, uint8_t bcb) {
+  for (uint16_t i=0; i<numTlc; i++)
+    setBrightness(i, bcr, bcg, bcb);
+}
+
+void Tlc59711::setChannel(uint16_t idx, uint16_t val) {
+  idx = 14*(idx/12) + idx%12;
+  if (idx < bufferSz)
+    buffer[idx] = val;
+}
+
+void Tlc59711::setRGB(uint16_t idx, uint16_t r, uint16_t g, uint16_t b) {
+  idx = 3*idx;
+  setChannel(idx, r);
+  setChannel(++idx, g);
+  setChannel(++idx, b);
+}
+void Tlc59711::setRGB(uint16_t r, uint16_t g, uint16_t b) {
+  for (uint16_t i=0, n=4*numTlc; i<n; i++)
+    setRGB(i, r, g, b);
+}
+
+static void reverseMemcpy(void *dst, void *src, size_t count) {
+  uint8_t* s = (uint8_t*)src;
+  while (count-- > 0)
+    *((uint8_t*)dst+count) = *s++;
+}
+
+void Tlc59711::xferSpi() {
+  reverseMemcpy(buffer2, buffer, bufferSz*2);
+  cli();
+  SPI.transfer(buffer2, bufferSz*2);
+}
+
+void Tlc59711::xferSpi16() {
+  cli();
+  for (int i=bufferSz-1; i >= 0; i--)
+    SPI.transfer16(buffer[i]);
+}
+
+void Tlc59711::xferShiftOut() {
+  if (noInterrupts)
+    cli();
+  for (int i=bufferSz-1; i >= 0; i--) {
+    uint16_t val = buffer[i];
+    shiftOut(dataPin, clkPin, MSBFIRST, val >> 8);
+    shiftOut(dataPin, clkPin, MSBFIRST, val);
+  }
+}
+
+void Tlc59711::write() {
+  if (!beginCalled)
+    return;
+  if (useSpi_) {
+    if (bufferXfer_)
+      xferSpi();
+    else
+      xferSpi16();
+  }
+  else
+    xferShiftOut();
+  sei();
+  // delay to make sure the TLC59711s read (latch) their shift registers;
+  // the delay required is 8 times the duration between the last two SCKI
+  // rising edges (plus 1.34 us); see datasheet pg. 22 for details
+  delayMicroseconds(postXferDelayMicros_);
+}
+
+void Tlc59711::end() {
+  if (beginCalled && useSpi_) {
+    SPI.endTransaction();
+    SPI.end();
+  }
+}
+
---
+++ Tlc59711.h
@@ -0,0 +1,94 @@
+//
+// Arduino library for controlling TI's TLC59711
+//
+// 21 Feb 2016 by Ulrich Stern
+//
+// notes:
+// * rewrite of the Adafruit_TLC59711 library:
+//  - avoids deprecated SPI library calls (making it, e.g., Teensy compatible)
+//  - significantly improved SPI performance
+//  - added API access to global brightness and TMGRST bit
+//  - added convenience functions that set values for all chips and LEDs
+//  - fixed some minor issues
+//
+
+#ifndef TLC59711_H
+#define TLC59711_H
+
+#include <Arduino.h>
+
+class Tlc59711 {
+
+public:
+  // object default state: (matches reset())
+  // - all grayscale (PWM) values 0
+  // - all brightness values 127 (maximum)
+  Tlc59711(uint16_t numTlc, uint8_t clkPin = 13, uint8_t dataPin = 11);
+
+  // select data transfer mode
+  // * beginFast():
+  //  - use hardware support (SPI library)
+  //  - use 10 MHz (TLC59711 datasheet value)
+  // * beginSlow():
+  //  - use software (shiftOut(), which is digitalWrite()-based)
+  // * if NOT used on an Arduino Uno or 10 MHz, postXferDelayMicros possibly
+  //  needs to be adjusted; see write() implementation and GitHub for details
+  void beginFast(bool bufferXfer = true, uint32_t spiClock = 10000000,
+    unsigned int postXferDelayMicros = 4);
+  void beginSlow(unsigned int postXferDelayMicros = 200,
+    bool interrupts = false);
+  // control of TMGRST bit
+  // * separate from begin() functions to reduce their parameters
+  // * side effect: resets all brightness values to maximum (127)
+  void setTmgrst(bool val = true);
+
+  // the following functions affect the state of this object only (the data
+  // is transferred to the chip when write() is called)
+  // * index order follows the datasheet
+  //   (note: chip 1 is the one directly connected to Arduino)
+  //  - channel: 0:R0 (chip 1), 1:G0, ..., 12:R0 (chip 2), ...
+  //  - RGB/LED: 0:R0,G0,B0 (chip 1), ..., 4:R0,G0,B0 (chip 2), ...
+  //  - tlcIdx: 0:chip 1, 1:chip 2, ...
+  // * functions without index set values for all chips and LEDs
+  // * brightness values (bcr, bcg, bcb) must be 0-127
+  void setChannel(uint16_t idx, uint16_t val);
+  void setRGB(uint16_t idx, uint16_t r, uint16_t g, uint16_t b);
+  void setRGB(uint16_t r, uint16_t g, uint16_t b);
+  void setLED(uint16_t idx, uint16_t r, uint16_t g, uint16_t b) {
+    setRGB(idx, r, g, b);
+  }
+    // setLED() is convenience function to allow faster testing/porting of
+    // code using Adafruit_TLC59711; the index order for multiple chips is
+    // different here, however, and chip 1 has the smallest indices
+  void setBrightness(uint16_t tlcIdx, uint8_t bcr, uint8_t bcg, uint8_t bcb);
+  void setBrightness(uint8_t bcr, uint8_t bcg, uint8_t bcb);
+  void resetBrightness() { setBrightness(127, 127, 127); }
+  void reset() {
+    setRGB(0, 0, 0);
+    resetBrightness();
+  }
+
+  // transfer data to TLC59711; will work only after a call to beginFast() or
+  // beginSlow()
+  void write();
+
+  // stop using this library
+  void end();
+
+private:
+  const uint16_t numTlc, bufferSz;
+  const uint8_t clkPin, dataPin;
+  uint16_t *buffer, *buffer2;
+  bool beginCalled;
+  uint8_t fc;
+  bool useSpi_, bufferXfer_, noInterrupts;
+  unsigned int postXferDelayMicros_;
+
+  void begin(bool useSpi, unsigned int postXferDelayMicros);
+  void xferSpi();
+  void xferSpi16();
+  void xferShiftOut();
+};
+
+#endif
+
